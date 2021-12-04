b1a546e7c59a9c2316aac44dc47a3d301c1814b7
Rashad Rivera <rashadrivera@gmail.com>
2021-12-04 00:28:29 -0500

    Add project files.

---
+++ CronExpression/CronExpression.cs
@@ -0,0 +1,265 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace System {
+
+	// https://crontab.guru/
+
+	public sealed class CronExpression {
+
+		private readonly MinuteParser[] _Minutes;
+
+		private readonly HoursParser[] _Hours;
+
+		private readonly DaysParser[] _Days;
+
+		private readonly MonthParser[] _Month;
+
+		private readonly DayOfWeekParser[] _DayOfWeek;
+
+		public CronExpression(string expression) {
+
+			_ValidateExpression(expression);
+
+			var matches = _GetMatches(expression.Trim());
+
+			this._Minutes = MinuteParser.Parse(matches.Result("${Minutes}"));
+			//this._Hours = new HoursParser(matches.Result("${Hours}"));
+			//this._Days = new DaysParser(matches.Result("${Days}"));
+			//this._Month = new MonthParser(matches.Result("${Month}"));
+			//this._DayOfWeek = new DayOfWeekParser(matches.Result("${DayOfWeek}"));
+		}
+
+		static void _ValidateExpression(string expression) {
+
+			Debug.Assert(!string.IsNullOrWhiteSpace(nameof(expression)), "Invalid method call");
+
+			if (string.IsNullOrEmpty(expression?.Trim()))
+				throw new ArgumentNullException(nameof(expression));
+
+			var match = _GetMatches(expression);
+
+			if (!match.Success)
+				throw new ArgumentException("Value is not a valid CRON expression", nameof(expression));
+		}
+
+		static Match _GetMatches(string expression)
+			=> Regex.Match(
+				expression,
+				"(?<Minutes>[0-9*,/-]+) (?<Hours>[0-9*,/-]+) (?<Days>[0-9*,/-]+) (?<Month>[0-9*,/\\-a-z]+) (?<DayOfWeek>[0-9*,/\\-a-z]+)",
+				RegexOptions.Singleline,
+				TimeSpan.FromSeconds(0.2)
+			);
+
+		public DateTimeOffset Next()
+			=> throw new NotImplementedException();
+
+		public DateTimeOffset NextInterval(int offset)
+			=> throw new NotImplementedException();
+
+		public TimeSpan Offset()
+			=> throw new NotImplementedException();
+
+		public TimeSpan OffsetInterval(int offset)
+			=> throw new NotImplementedException();
+
+		#region Member Class(es)
+
+		sealed class MinuteParser {
+
+			readonly IBaseValue _Value;
+
+			public MinuteParser(string minutePart) {
+
+				if (string.IsNullOrEmpty(minutePart))
+					throw new ArgumentNullException(nameof(minutePart));
+
+				var rangeRegex = Regex.Match(minutePart, @"^(?<Min>\d+)-(?<Max>\d+)$");
+				var stepsRegex = Regex.Match(minutePart, @"^(?<Start>\d+)/(?<Step>\d+)$");
+				if (minutePart == "*")
+					this._Value = new AnyValue();
+				else if (rangeRegex.Success) {
+					var min = int.Parse(rangeRegex.Result("${Min}"));
+					var max = int.Parse(rangeRegex.Result("${Max}"));
+					this._ValidateValue(min);
+					this._ValidateValue(max);
+					this._Value = new RangeValue(min, max);
+				} else if (stepsRegex.Success) {
+					var start = int.Parse(rangeRegex.Result("${Start}"));
+					var step = int.Parse(rangeRegex.Result("${Step}"));
+					this._ValidateValue(start);
+					this._ValidateValue(step);
+					this._Value = new StepValue(this._ExpandSteps(start, step));
+				} else if (int.TryParse(minutePart, out var specificValue)) {
+					this._ValidateValue(specificValue);
+					this._Value = new SpecificValue(minutePart);
+				} else
+					throw new FormatException($"'{minutePart}' is invalid");
+			}
+
+			public static MinuteParser[] Parse(string minuteParts) {
+
+				var match = Regex.Match(minuteParts, @"(?<value>[-\*0-9\/]+)(?:,(?<value>[-\*0-9\/]+))*");
+				if (!match.Success)
+					throw new FormatException($"'{minuteParts}' is invalid");
+
+				var returnValue = new List<MinuteParser>();
+				foreach (var item in minuteParts.Split(','))
+					returnValue.Add(new MinuteParser(item));
+				return returnValue.ToArray();
+			}
+
+			#region Helper Method(s)
+
+			object[] _ExpandSteps(int start, int step) {
+
+				var returnValue = new List<object>();
+				returnValue.Add(start);
+				var next = start;
+				do {
+					returnValue.Add(next);
+					next += step;
+				} while (next < 59);
+				return returnValue.ToArray();
+			}
+
+			void _ValidateValue(int value) {
+				if (value < 0 || value > 59)
+					throw new FormatException($"Value '{value}' is out of range");
+			}
+
+			#endregion
+		}
+
+		sealed class HoursParser {
+
+			readonly string _MinutePart;
+
+			public HoursParser(string minutePart) {
+
+				if (string.IsNullOrEmpty(minutePart))
+					throw new ArgumentNullException(nameof(minutePart));
+				if (!Validate(minutePart))
+					throw new ArgumentException("Value is not valid", nameof(minutePart));
+				this._MinutePart = minutePart;
+				this.IsValid = false;
+			}
+
+			public bool IsValid { get; }
+
+			public static bool Validate(string expression)
+				=> false;
+		}
+
+		sealed class DaysParser {
+
+			readonly string _MinutePart;
+
+			public DaysParser(string minutePart) {
+
+				if (string.IsNullOrEmpty(minutePart))
+					throw new ArgumentNullException(nameof(minutePart));
+				if (!Validate(minutePart))
+					throw new ArgumentException("Value is not valid", nameof(minutePart));
+				this._MinutePart = minutePart;
+				this.IsValid = false;
+			}
+
+			public bool IsValid { get; }
+
+			public static bool Validate(string expression)
+				=> false;
+		}
+
+		sealed class MonthParser {
+
+			readonly string _MinutePart;
+
+			public MonthParser(string minutePart) {
+
+				if (string.IsNullOrEmpty(minutePart))
+					throw new ArgumentNullException(nameof(minutePart));
+				if (!Validate(minutePart))
+					throw new ArgumentException("Value is not valid", nameof(minutePart));
+				this._MinutePart = minutePart;
+				this.IsValid = false;
+			}
+
+			public bool IsValid { get; }
+
+			public static bool Validate(string expression)
+				=> false;
+		}
+
+		sealed class DayOfWeekParser {
+
+			readonly string _MinutePart;
+
+			public DayOfWeekParser(string minutePart) {
+
+				if (string.IsNullOrEmpty(minutePart))
+					throw new ArgumentNullException(nameof(minutePart));
+				if (!Validate(minutePart))
+					throw new ArgumentException("Value is not valid", nameof(minutePart));
+				this._MinutePart = minutePart;
+				this.IsValid = false;
+			}
+
+			public bool IsValid { get; }
+
+			public static bool Validate(string expression)
+				=> false;
+		}
+
+		interface IBaseValue {
+
+			object[] Values();
+		}
+
+		sealed class AnyValue : IBaseValue {
+
+			public object[] Values()
+				=> null;
+		}
+
+		sealed class RangeValue : IBaseValue {
+
+			readonly object _Min;
+
+			readonly object _Max;
+
+			public RangeValue(object min, object max) {
+				this._Min = min ?? throw new ArgumentNullException(nameof(min));
+				this._Max = max ?? throw new ArgumentNullException(nameof(max));
+			}
+
+			public object[] Values()
+				=> new object[] { this._Min, this._Max };
+		}
+
+		sealed class SpecificValue : IBaseValue {
+
+			readonly object _Value;
+
+			public SpecificValue(object value)
+				=> this._Value = value;
+
+			public object[] Values()
+				=> new object[] { this._Value };
+		}
+
+		sealed class StepValue : IBaseValue {
+
+			readonly object[] _AllSteps;
+
+			public StepValue(params object[] allSteps)
+				=> this._AllSteps = allSteps ?? throw new ArgumentNullException(nameof(allSteps));
+
+			public object[] Values()
+				=> this._AllSteps;
+		}
+
+		#endregion
+	}
+}
---
+++ CronExpressionTests/ConstructionTests.cs
@@ -0,0 +1,79 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace CronExpressionTests {
+
+	[TestClass]
+	[TestCategory("Construction")]
+	public class ConstructionTests {
+
+		[TestMethod]
+		[TestProperty("Type", "Positive")]
+		public void DefaultConstructor()
+			=> new CronExpression("* * * * *");
+
+		[TestMethod]
+		[TestProperty("Type", "Negative")]
+		public void NullConstructionTest() {
+
+			bool hasFailed = false;
+			try {
+				new CronExpression(null);
+			} catch {
+				hasFailed = true;
+			}
+
+			Assert.IsTrue(hasFailed, $"Value '' should not be permitted");
+		}
+
+		[TestMethod]
+		[TestProperty("Type", "Negative")]
+		public void EmptyStringConstructionTest() {
+
+			bool hasFailed = false;
+			try {
+				new CronExpression(string.Empty);
+			} catch {
+				hasFailed = true;
+			}
+
+			Assert.IsTrue(hasFailed, $"Value '' should not be permitted");
+		}
+
+		[DataTestMethod]
+		[DataRow(" ")]
+		[DataRow("*")]
+		[DataRow("* *")]
+		[DataRow("* * *")]
+		[DataRow("* * * *")]
+		[DataRow("-1 * * * *")]
+		[DataRow(", * * * *")]
+		[DataRow("- * * * *")]
+		[DataRow("/ * * * *")]
+		[DataRow("60 * * * *")]
+		[DataRow("100 * * * *")]
+		[DataRow("0 24 * * *")]
+		[TestProperty("Type", "Negative")]
+		public void BadExpressionConstructionTest(string value) {
+
+			bool hasFailed = false;
+			try {
+				new CronExpression(value);
+			} catch {
+				hasFailed = true;
+			}
+
+			Assert.IsTrue(hasFailed, $"Value '{value}' should not be permitted");
+		}
+
+		[DataTestMethod]
+		[DataRow("* * * * *")]
+		[DataRow("10 * * * *")]
+		[DataRow("10-15 * * * *")]
+		[DataRow("1/3 * * * *")]
+		[DataRow("1/3,0,1-5 * * * *")]
+		[TestProperty("Type", "Positive")]
+		public void ValidExpressionsConstructionTest(string value)
+			=> new CronExpression(value);
+	}
+}
